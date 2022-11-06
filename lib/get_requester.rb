b3cb37a6a932704379a044018677a10153e6c26e
calvin <ncalvin67@gmail.com>
2022-11-06 17:18:01 +0300

    commit tree c3f8e70925c37232a7a31e6d5333dcc4141072e2

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,31 +1,23 @@
-# Write your code here
 require 'net/http'
 require 'open-uri'
 require 'json'
- 
-class GetPrograms
+
+class GetRequester
 
   def initialize(url)
     @url = url
   end
 
-  def get_programs
+#  URL = " https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+ def get_response_body
     uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
     response.body
-  end
+ end
 
-  def program_school
-    # we use the JSON library to parse the API response into nicely formatted JSON
-      programs = JSON.parse(self.get_programs)
-      programs.collect do |program|
-        program["agency"]
-      end
-    end
+ def parse_json
+  JSON.parse(get_response_body)
+ end
 
 end
-
-programs = GetPrograms.new
-puts programs.program_school.uniq
-# programs = GetPrograms.new.get_programs
-# puts programs
